31e33009641051c7ff5dbe5b41b62c2210d80cfa
Sid <n10441468@qut.edu.au>
2020-05-27 22:56:36 +1000

    read xml file does scape everything fron xml file

--- BillboardControlPanel/src/ReadXMLFile.java
+++ BillboardControlPanel/src/ReadXMLFile.java
@@ -20,10 +20,10 @@ public class ReadXMLFile {
 
         NodeList list = document.getElementsByTagName("billboard");
 
-        for(int i =0 ;i < list.getLength();i++){
+        for (int i = 0; i < list.getLength(); i++) {
             Node node = list.item(i);
 
-            if(node.getNodeType() == Node.ELEMENT_NODE){
+            if (node.getNodeType() == Node.ELEMENT_NODE) {
                 Element element = (Element) node;
                 String background = element.getAttribute("background");
                 String message = element.getElementsByTagName("message").item(0).getTextContent();
@@ -36,6 +36,48 @@ public class ReadXMLFile {
 
             }
         }
+
+        NodeList list_1 = document.getElementsByTagName("message");
+
+        for (int i = 0; i < list_1.getLength(); i++) {
+            Node node_1 = list_1.item(i);
+
+            if (node_1.getNodeType() == Node.ELEMENT_NODE) {
+                Element element_1 = (Element) node_1;
+                String colour_message = element_1.getAttribute("colour");
+                System.out.println(colour_message);
+            }
+        }
+
+
+        NodeList list_2 = document.getElementsByTagName("information");
+
+        for (int i = 0; i < list_2.getLength(); i++) {
+            Node node_2 = list_2.item(i);
+
+            if (node_2.getNodeType() == Node.ELEMENT_NODE) {
+                Element element_2 = (Element) node_2;
+                String colour_info = element_2.getAttribute("colour");
+                System.out.println(colour_info);
+            }
+        }
+
+        NodeList list_3 = document.getElementsByTagName("picture");
+
+        for (int i = 0; i < list_3.getLength(); i++) {
+            Node node_3 = list_3.item(i);
+
+            if (node_3.getNodeType() == Node.ELEMENT_NODE) {
+                Element element_3 = (Element) node_3;
+                String image_data = element_3.getAttribute("data");
+                String image_url = element_3.getAttribute("url");
+
+                System.out.println(image_data);
+                System.out.println(image_url);
+            }
+        }
+
+
     }
 
     public static void main(String[] args) throws IOException, SAXException, ParserConfigurationException {
