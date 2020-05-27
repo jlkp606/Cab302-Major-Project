1709caec3109e0c4bab16f63f6c74d811b427c4b
Jetrin Anderson <jetrinanderson@gmail.com>
2020-05-27 22:39:53 +1000

    tidied up error screen

---
+++ BillboardViewer/src/Viewer/BillboardGenerator.java
@@ -0,0 +1,51 @@
+package Viewer;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+
+public class ReadXMLFile {
+    public static final String xmlFilePath = System.getProperty("user.dir")+"/xmlFile.xml";
+    public void read() throws ParserConfigurationException, IOException, SAXException {
+        DocumentBuilderFactory documentFactory = DocumentBuilderFactory.newInstance();
+
+        DocumentBuilder documentBuilder = documentFactory.newDocumentBuilder();
+
+        Document document = documentBuilder.parse(xmlFilePath);
+
+        NodeList list = document.getElementsByTagName("billboard");
+
+        for(int i =0 ;i < list.getLength();i++){
+            Node node = list.item(i);
+
+            if(node.getNodeType() == Node.ELEMENT_NODE){
+                Element element = (Element) node;
+                String background = element.getAttribute("background");
+                String message = element.getElementsByTagName("message").item(0).getTextContent();
+                String info = element.getElementsByTagName("information").item(0).getTextContent();
+//                String colour = element.getAttribute("data");
+//                String picture = element.getElementsByTagName("picture").item(0).getTextContent();
+                String colour = element.getElementsByTagName("message").item(0).getTextContent();
+
+//                String background = element.getAttribute("background");
+//                String background = element.getAttribute("background");
+//                String background = element.getAttribute("background");
+                System.out.println(background);
+                System.out.println(message);
+                System.out.println(info);
+                System.out.println(colour);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException, SAXException, ParserConfigurationException {
+        ReadXMLFile file = new ReadXMLFile();
+        file.read();
+    }
+}
--- BillboardViewer/src/Viewer/BillboardViewer.java
+++ BillboardViewer/src/Viewer/BillboardViewer.java
@@ -1,10 +1,24 @@
 package Viewer;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import java.awt.*;
 import java.awt.event.*;
+import java.io.File;
+import java.io.IOException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
 
 public class BillboardViewer {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
         JFrame frame = new JFrame("Billboard Viewer");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
@@ -46,11 +60,30 @@ public class BillboardViewer {
                 }
             }
         };
+
+        File file = new File("F:\\XMLFile.xml");
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document document = builder.parse(new File("Employees.xml"));
+
         frame.addMouseListener(mouseexit);
         frame.addKeyListener(escapeexit);
-        frame.getContentPane().add(new JLabel(" HEY!!!"));
+        JLabel ErrorMessage = new JLabel("Error: No XML file found to display.");
+        ErrorMessage.setFont(new Font("Century Schoolbook", Font.PLAIN, 48));
+        ErrorMessage.setForeground(Color.WHITE);
+        ErrorMessage.setHorizontalAlignment(JLabel.CENTER);
+        frame.getContentPane().add(ErrorMessage);
+        frame.getContentPane().setBackground(Color.BLUE);
         frame.setUndecorated(true);
         frame.setVisible(true);
         frame.setFocusable(true);
+
+/*        HashMap<String, Object> request = new HashMap<>();
+        request.put("display, billboard");
+        Socket socket = getClientSocket();
+        sendRequest(socket,request);
+        HashMap<String,Object> response = getResponse(socket);
+        Billboard billboard = res.get("billboard");
+        socket.close();*/
     }
 }
