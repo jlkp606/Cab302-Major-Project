cc7c5c9a5535422c6b4edd6911581587cd333269
Liam <n10216723@qut.edu.au>
2020-05-28 18:35:18 +0900

    Added User database and and get, set and delete functions

--- BillboardServer/src/Database/DatabaseSource.java
+++ BillboardServer/src/Database/DatabaseSource.java
@@ -7,6 +7,29 @@ public interface DatabaseSource {
     /**
      * Adds a billboard to the billboard table, if it is not already in the table
      *
+     * @param u  Billboard to add
+     */
+    void addUser(DataBase.User u);
+
+    /**
+     * Extracts all the details of a Billboard from the table based on the
+     * name passed in.
+     *
+     * @param username The name of the billboard as a String to search for.
+     * @return all details in a Person object for the name
+     */
+    DataBase.User getUser(String username);
+
+    /**
+     * Deletes a USER from the database.
+     *
+     * @param name The name to delete from the table.
+     */
+    void deleteUser(String name);
+
+    /**
+     * Adds a billboard to the billboard table, if it is not already in the table
+     *
      * @param b  Billboard to add
      */
     void addBillboard(Billboard b);
